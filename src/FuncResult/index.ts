f52b201744973589906efa7d73ff38d34dcbf693
shimopino <shimoke4869@gmail.com>
2022-11-30 02:21:11 +0900

    関数群をResultモジュールとして出力する

--- src/FuncResult/index.ts
+++ src/FuncResult/index.ts
@@ -1,10 +1,16 @@
 import { map } from './map';
 import { mapErr } from './mapErr';
 import { andThen } from './andThen';
+import { apply } from './apply';
+import { traverseA } from './traverseA';
+import { traverseM } from './traverseM';
 export * from './result';
 
 export const Result = {
   map,
   mapErr,
   andThen,
+  apply,
+  traverseA,
+  traverseM,
 };
